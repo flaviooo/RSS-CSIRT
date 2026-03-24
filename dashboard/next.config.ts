2bf5ca89d7eeb7bd0c3822ae1281edc76a8e739d
fL <flaviooo@gmail.com>
2026-03-24 18:45:01 +0100

    Auth

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
       hmrRefreshes: false,
     },
   },
-  devServer: {
-    allowedDevOrigins: ['http://controllo1.csea.local'], // Sostituisci con il tuo dominio o origine
-  },
 };
 
 export default nextConfig;
