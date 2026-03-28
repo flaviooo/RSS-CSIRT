4c7e6662a6e99bfe850404d8111c0b6788366014
fL <flaviooo@gmail.com>
2026-03-28 01:25:48 +0100

    pm2 ecosystem

--- dashboard/ecosystem.config.js
+++ dashboard/ecosystem.config.js
@@ -3,17 +3,22 @@ require('dotenv').config({ path: '.env' });
 module.exports = {
   apps: [
     {
-      name: 'RSS-CSIRT', // Nome dell'app
-      script: 'node_modules/next/dist/bin/next', // Percorso al binario di Next.js
-      args: 'start', // Comando per avviare Next.js in produzione
-      env: {
-        PORT: process.env.PORT || 3006,
-        NODE_ENV: 'production' // Ambiente di produzione
-      },
-      out_file: 'log/out.log', // Log di output
-      error_file: 'log/err.log', // Log di errori
-      log_date_format: 'YYYY-MM-DD HH:mm Z', // Formato data nei log
-      time: true // Includi la durata nei log
+      name: 'Dashboard',
+      script: 'npm',
+      args: 'run start -- -p 3006',
+      cwd: '.',
+      output: 'log/DashboardOut.log',
+      error: 'log/DashboardErr.log',
+      time: true,
+    },
+    {
+      name: 'DashboardCron',
+      script: 'npm',
+      args: 'run cron',
+      cwd: '.',
+      output: 'log/CronOut.log',
+      error: 'log/CronErr.log',
+      time: true,
     }
   ]
 };
